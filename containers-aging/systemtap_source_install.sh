02cb19016f9305ddd1e9609f5bce8039e23eb7ab
ThaysonScript <thaysonguedes01@gmail.com>
2024-04-27 16:19:48 -0300

    systemtap instalation

--- containers-aging/systemtap_source_install.sh
+++ containers-aging/systemtap_source_install.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+reset
+[[ -d "/root/systemtap" ]] && {
+    echo "A pasta do systemtap já existe, talvés você já o tenha configurado"
+    echo "Caso não, apague a pasta e execute novamente"
+    exit 0
+}
+
 echo "Verifique se você está no diretorio /root: "; echo "Seu dir atual é: $(pwd)"; echo "Caso não, mude para ele!"
 
 GET_SYSTEMTAP_SOURCE() {
@@ -11,17 +18,18 @@ GET_SYSTEMTAP_SOURCE() {
         echo "erro ao tentar baixar o codigo fonte do systemtap a partir do repositório git"; echo "saindo..."; exit 1
     }
 
+    mv systemtap /root || {
+        echo "erro ao mover systemtap para o dir /root"
+    }
+
     cd "/root/systemtap" || {
         echo "erro ao tentar entrar no diretorio do systemtap"; echo "saindo..."; exit 1
     }
 
+    configurar=$(./configure | grep -o "./configure.*prefix=/root/systemtap-[0-9]\+\.[0-9]\+\-[0-9]\+")
 
-    ./configure || {
-        echo "Este erro é proposital, copie e rode o ./configure com uma saida parecida com esta abaixo:"
-
-        # shellcheck disable=SC2154
-        echo "./configure 'PKG_CONFIG_PATH=/usr/lib/pkgconfig' python=':' pyexecdir='' python3='/usr/bin/python3' py3execdir='${exec_prefix}/lib/python3.11/site-packages' --prefix=/root/systemtap-5.0-8684"
-        echo "saindo..."; exit 0
+    eval "$configurar" || {
+        echo "Error em fazer ./configure "; echo "saindo..."; exit 1
     }
 
     make all || {
@@ -33,15 +41,17 @@ GET_SYSTEMTAP_SOURCE() {
     }
 
     echo "adicionando path do systemtap em /root/.bashrc"; echo "export PATH=\$PATH:/root/systemtap" >> "/root/.bashrc"
+    source /root/.bashrc
+    echo "$PATH"
 
-    echo "faca: source /root/.bashrc"; echo "verifique se a path foi adicionada com: echo $'PATH'"
+    printf "\n%s\n" "Caso o systemtap não seja caregado, faca: source /root/.bashrc"; echo "verifique se a path foi adicionada com: echo \$PATH"
 
     echo "Caso deseje executar como user sudo ou comum, faça: ( echo export PATH=\$PATH:/home/$(logname)/systemtap >> /home/$(logname)/.bashrc ) e faça ( source /home/$(logname)/.bashrc )"
-
-    echo "concluido!!!"; exit 0
 }
 
 RUNNING_SMALL_SYSTEMTAP_TEST() {
+    print "\n\n\n"
+    echo "Rodando teste de instalação do systemtap"
     # shellcheck disable=SC2317
     stap -ve 'probe begin { log("hello world") exit () }'
 
@@ -50,7 +60,6 @@ RUNNING_SMALL_SYSTEMTAP_TEST() {
 }
 
 MAIN() {
-    reset
     GET_SYSTEMTAP_SOURCE
 
     # shellcheck disable=SC2317
