3cc32a95ea0a143746d9db6d17de4a714863a054
ThaysonScript <thaysonguedes01@gmail.com>
2024-07-10 23:43:30 -0300

    update - testing-softwares/get-git-softwares/teastore-no-containers.sh

--- testing-softwares/get-git-softwares/teastore-no-containers.sh
+++ testing-softwares/get-git-softwares/teastore-no-containers.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 # TEASTORE-NO-CONTAINERS:MAIN
+rm -rf ../teastore-no-containers
 git clone "https://github.com/ThaysonScript/teastore-no-containers.git"
+mv teastore-no-containers ../teastore-no-containers
