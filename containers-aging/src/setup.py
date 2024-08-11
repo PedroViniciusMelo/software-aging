182f03e61aa05256cae8a22040e7e02a20cb6b71
PedroViniciusMelo <pedro.vinicius.melo.silva@gmail.com>
2024-08-11 16:21:47 -0300

    added option to not use app start time

--- containers-aging/src/setup.py
+++ containers-aging/src/setup.py
@@ -13,7 +13,9 @@ class Setup:
 
     def build_images(self):
         for container in self.containers:
+            print(f"Building image {container['name']}:{container['version']}")
             execute_command(f"{self.software} pull docker.io/{container['name']}:{container['version']}")
             execute_command(f"{self.software} save {container['name']}:{container['version']} > {self.path}/{container['name']}.tar")
             execute_command(f"{self.software} save {container['name']}:{container['version']} > {self.path}/temp_{container['name']}.tar")
             execute_command(f"{self.software} rmi {container['name']}:{container['version']}")
+            print(f"Done!")
